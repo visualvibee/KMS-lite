3c2f414c98bd6bede9ae17a56aca82a465ed4696
visualvibee <aditi0011aditi@gmail.com>
2026-07-14 15:42:14 +0530

    docker files

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="KMS-Lite", version="2.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
